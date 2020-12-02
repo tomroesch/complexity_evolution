436e84d0f92d4db17d0625c19da404033478461d
tomroesch <troeschi@caltech.edu>
2020-12-01 20:17:46 -0800

    fixed error in simulation

--- code/scripts/script1_sub.jl
+++ code/scripts/script1_sub.jl
@@ -19,13 +19,13 @@ end
 end
 
 # Parameters
-reps = 500
+reps = 1000
 steps = 1 * 10^6
 N = 1000
 nu = 0
 emat = 2 * (ones(4, 4) - Matrix{Float64}(I, 4, 4))
 rho_array = [0, 0.1, 0.5, 1]      # Driver mutation rates
-l_array = collect(5:30)           # Binding site lengths
+l_array = [10, 15, 20]           # Binding site lengths
 f0_array = [20, 50, 100] ./ 2N    # Fitness scales
 fl = 0
 
@@ -79,7 +79,7 @@ end
         for l in 1:length(l_array)
             for r in 1:length(f0_array)
                 f = Jevo.fermi_fitness(f0=f0_array[r], l=l_array[l])
-                E = run(N, f0_array[r], fl, rho_array[i], nu, l_array[l], emat, steps)
+                Gamma_results[i, l, r, j] = run(N, f0_array[r], fl, rho_array[i], nu, l_array[l], emat, steps)
                 RHO[i, l, r, j] = rho_array[i]
                 L[i, l, r, j] = l_array[l]
                 F0[i, l, r, j] = f0_array[r]
